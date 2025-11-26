5fe0067ba5d394504586ed11e605b8c022472322
Beto <vargasmorenoalberto@gmail.com>
2025-11-26 16:17:59 -0600

    WIP. reading and showing existing files
    - test reading json file
    - test receiving POST requests

--- api/myapp.php
+++ api/myapp.php
@@ -3,6 +3,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
+function isJson(string $string): bool {
+   json_decode($string);
+   return json_last_error() === JSON_ERROR_NONE;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     echo $email;
@@ -15,6 +20,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $default_response = [
     "message" => "hi, there!"
 ];
-$json_response = json_encode($default_response);
+
+$fileReaded = file_get_contents('notes.json');
+
+$comments = isJson($fileReaded)
+    ? json_decode($fileReaded, true)
+    : [];
+(!array_key_exists('comments', $comments)) && die("error decoding readed data");
+
+/*
+foreach ($comments['comments'] as $idx => $comment) {
+    $note = $comment['comment'];
+}
+
+*/
+
+$json_response = json_encode($comments);
 header('Content-type:application/json');
 echo $json_response;
