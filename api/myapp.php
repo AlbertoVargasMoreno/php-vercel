aadc21a0ba2a3990cd0e4d3a31eafc6838f8a0a5
Beto <vargasmorenoalberto@gmail.com>
2025-11-26 17:00:46 -0600

    feat: use file as storage
    - add logic to read, update, and write storage file
    - this demonstrates alternative to persistent storage without DB
    - needs to be tested in vercel

--- api/myapp.php
+++ api/myapp.php
@@ -8,33 +8,46 @@ function isJson(string $string): bool {
    return json_last_error() === JSON_ERROR_NONE;
 }
 
+function appReadFile(string $filename = "notes.json"): array {
+    $fileReaded = file_get_contents($filename);
+    $comments = isJson($fileReaded)
+        ? json_decode($fileReaded, true)
+        : [];
+    (!array_key_exists('comments', $comments)) && die("error decoding readed data");
+    return $comments;
+}
+
+function updateContent(array $comments, array $data) : string {
+    $comments['comments'][] = $data;
+    $json_response = json_encode($comments);
+    return $json_response;
+}
+
+function writeFile(string $filename = "notes.json", string $content = "\n") : void {
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    fwrite($myfile, $content);
+    fclose($myfile);
+}
+
+$dataResponse = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    echo $email;
-    // Takes raw data from the request
+    // $email = $_POST['email'];
+    // echo $email;
     $json = file_get_contents('php://input');
-    // Converts it into a PHP object
-    $data = json_decode($json);
+    $data = json_decode($json, true);
+
+    $comments = appReadFile();
+    $content = updateContent($comments, $data);
+    writeFile(content: $content);
+
+    $dataResponse = json_decode($content);
 }
 
 $default_response = [
-    "message" => "hi, there!"
+    "message" => "hi, there!",
+    "data" => $dataResponse
 ];
 
-$fileReaded = file_get_contents('notes.json');
-
-$comments = isJson($fileReaded)
-    ? json_decode($fileReaded, true)
-    : [];
-(!array_key_exists('comments', $comments)) && die("error decoding readed data");
-
-/*
-foreach ($comments['comments'] as $idx => $comment) {
-    $note = $comment['comment'];
-}
-
-*/
-
-$json_response = json_encode($comments);
+$json_response = json_encode($default_response);
 header('Content-type:application/json');
 echo $json_response;
